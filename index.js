d008c180a1c416bc7a41b0aefa269947603746a2
Gilberto Medero <gjmedero@gmail.com>
2023-03-12 17:43:46 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const copyCat = [...cats, name];
+    return copyCat;
+}
+
+function prependCat(name) {
+    const copyCat = [name, ...cats];
+    return copyCat;
+}
+
+function removeLastCat() {
+    const copyCats = cats.slice(0, -1);
+    return copyCats;
+}
+
+function removeFirstCat() {
+    const copyCat = cats.slice(1,);
+    return copyCat;
+}
